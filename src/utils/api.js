94385eddb333a7c1f7c5136d20b1923cab3ed8a3
Jaime Convery <jaimeconvery@gmail.com>
2022-11-10 17:17:58 -0500

    refactor: add import to api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,5 @@
 import firebase from '../firebase';
+import 'firebase/database'
 
 // Generate a unique token for storing user books data on the backend server.
 let userID = localStorage.userID;
@@ -8,62 +9,62 @@ const booksRef = firebase.database().ref('users/' + userID + '/books/');
 
 // get snapshot of all books as a Promise
 const getData = (ref) => {
-	return new Promise((res, rej) => {
-		const onError = (error) => rej(error);
-		const onData = (snap) => res(snap.val());
+    return new Promise((res, rej) => {
+        const onError = (error) => rej(error);
+        const onData = (snap) => res(snap.val());
 
-		ref.on('value', onData, onError);
-	});
+        ref.on('value', onData, onError);
+    });
 };
 
 /* get books from Firebase and return null or books object */
 export const getBooks = () => {
-	// consume the promise of getData()
-	const books = getData(booksRef)
-		.then((value) => {
-			return value;
-		})
-		.catch((error) => alert('Having trouble getting books. Please try again later.'));
-	return books;
+    // consume the promise of getData()
+    const books = getData(booksRef)
+        .then((value) => {
+            return value;
+        })
+        .catch((error) => alert('Having trouble getting books. Please try again later.'));
+    return books;
 };
 
 /* search books using the Google Books API */
 export const searchBook = (query) => {
-	return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
-		.then((res) => res.json())
-		.then((data) => data.items)
-		.catch((error) => alert('Having trouble fetching books. Please try again later.'));
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
+        .then((res) => res.json())
+        .then((data) => data.items)
+        .catch((error) => alert('Having trouble fetching books. Please try again later.'));
 };
 
 /* save the added book to Firebase 
 https://firebase.google.com/docs/database/web/read-and-write#basic_write
 */
 export const saveBook = (book) => {
-	// generate new key and set book
-	return new Promise((res, rej) => res(booksRef.push().set(book)));
+    // generate new key and set book
+    return new Promise((res, rej) => res(booksRef.push().set(book)));
 };
 
 /* update book comment to Firebase 
 https://firebase.google.com/docs/database/web/read-and-write#update_specific_fields
 */
 export const updateComment = (id, comment) => {
-	// get the key of the book
-	return firebase
-		.database()
-		.ref(booksRef)
-		.orderByChild('id')
-		.equalTo(id)
-		.once('value')
-		.then((snapshot) => {
-			let value = snapshot.val();
-			if (value) {
-				var key = Object.keys(value)[0];
-				// update the comment field of book
-				return new Promise((res, rej) => {
-					var updates = {};
-					updates['/users/' + userID + '/books/' + key + '/comment/'] = comment;
-					return res(firebase.database().ref().update(updates));
-				});
-			}
-		});
+    // get the key of the book
+    return firebase
+        .database()
+        .ref(booksRef)
+        .orderByChild('id')
+        .equalTo(id)
+        .once('value')
+        .then((snapshot) => {
+            let value = snapshot.val();
+            if (value) {
+                var key = Object.keys(value)[0];
+                // update the comment field of book
+                return new Promise((res, rej) => {
+                    var updates = {};
+                    updates['/users/' + userID + '/books/' + key + '/comment/'] = comment;
+                    return res(firebase.database().ref().update(updates));
+                });
+            }
+        });
 };
