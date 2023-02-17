3a15daadeff44cc06f8cc3cf68520fcf4ac88d8c
Jaime Convery <jaimeconvery@gmail.com>
2023-02-17 15:42:30 -0500

    refactor: remove uneeded reducer calls

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import { sortByAuthor, sortByDate, filterByValue } from './../actions/books';
+import { sortByDate, sortByAuthor, filterByValue } from './../actions/books';
 import { connect } from 'react-redux';
 import Book from './../components/Book';
 import './bookList.css';
@@ -10,79 +10,60 @@ import { exportBooks } from '../utils/helper';
  * @description Display list of books user has read 
  */
 class BookList extends Component {
-	componentDidUpdate(prevProps, _) {
-		// if page is changed and there are filters, refresh the books to apply the filter - can only be one
-		if (
-			prevProps.books.currentPage !== this.props.books.currentPage &&
-			this.props.books.appliedFilters.length > 0
-		) {
-			const filter = this.props.books.appliedFilters[0];
-			switch (filter) {
-				case 'SORT_BY_DATE':
-					this.props.sortByDate();
-					break;
+    componentDidUpdate(prevProps, _) {
+        // if filter by value was applied but is now not applied and there are still filters, refresh the book list to apply the filter
+        if (
+            prevProps.books.appliedFilters.includes('FILTER_BY_VALUE') &&
+            !this.props.books.appliedFilters.includes('FILTER_BY_VALUE') &&
+            this.props.books.appliedFilters.length > 0
+        ) {
+            const filter = this.props.books.appliedFilters[0];
+            switch (filter) {
+                case 'SORT_BY_DATE':
+                    this.props.sortByDate();
+                    break;
 
-				case 'SORT_BY_AUTHOR':
-					this.props.sortByAuthor();
-					break;
+                case 'SORT_BY_AUTHOR':
+                    this.props.sortByAuthor();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
 
-				default:
-					break;
-			}
-		}
-		// if filter by value was applied but is now not applied and there are still filters, refresh the book list to apply the filter - can only be one
-		if (
-			prevProps.books.appliedFilters.includes('FILTER_BY_VALUE') &&
-			!this.props.books.appliedFilters.includes('FILTER_BY_VALUE') &&
-			this.props.books.appliedFilters.length > 0
-		) {
-			const filter = this.props.books.appliedFilters[0];
-			switch (filter) {
-				case 'SORT_BY_DATE':
-					this.props.sortByDate();
-					break;
-
-				case 'SORT_BY_AUTHOR':
-					this.props.sortByAuthor();
-					break;
-				default:
-					break;
-			}
-		}
-	}
-
-	render() {
-		const { books, filteredBooks, filteredPages } = this.props.books;
-		return (
-			<Fragment>
-				<button
-					style={{ margin: '0px' }}
-					class="btn btn"
-					onClick={() => exportBooks(books)}>
-					Export Books
-				</button>
-				{filteredPages > 0 && <Pagination />}
-				<div className="books-grid">
-					{filteredBooks && filteredBooks !== null && filteredBooks.length > 0 ? (
-						filteredBooks.map((book, index) => (
-							<Book key={index} book={book} id={book.id} />
-						))
-					) : (
-						<h1>No Books</h1>
-					)}
-				</div>
-				{filteredPages > 0 && <Pagination />}
-			</Fragment>
-		);
-	}
+    render() {
+        const { books, filteredBooks, filteredPages } = this.props.books;
+        return (
+            <Fragment>
+                <button
+                    style={{ margin: '0px' }}
+                    className="btn btn"
+                    onClick={() => exportBooks(books)}>
+                    Export Books
+                </button>
+                {filteredPages > 0 && <Pagination />}
+                <div className="books-grid">
+                    {filteredBooks && filteredBooks !== null && filteredBooks.length > 0 ? (
+                        filteredBooks.map((book, index) => (
+                            <Book key={index} book={book} id={book.id} />
+                        ))
+                    ) : (
+                        <h1>No Books</h1>
+                    )}
+                </div>
+                {filteredPages > 0 && <Pagination />}
+            </Fragment>
+        );
+    }
 }
 
 export default connect(
-	({ books, loadingBar }) => {
-		return {
-			books,
-			loading: loadingBar
-		};
-	},
-	{ sortByAuthor, sortByDate, filterByValue }
+    ({ books, loadingBar }) => {
+        return {
+            books,
+            loading: loadingBar
+        };
+    },
+    { sortByAuthor, sortByDate, filterByValue }
 )(BookList);
